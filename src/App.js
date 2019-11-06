ff64ab16786f59ecd4c2e85dba330b97fbece438
Oguz <me@oguzoeztuerk.com>
2019-11-07 00:59:24 +0100

    solved scrolltotop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import ScrollToTop from "react-router-scroll-top";
 
 import { postMDBConfig } from "./actions/PostMDBConfigAction";
 import postMovieGenres from "./actions/movieActions/postMovieGenres";
@@ -11,8 +12,6 @@ import LandingPage from "./pages/landingPage/landingPage";
 import Discover from "./pages/discover/discover";
 import ItemDetails from "./pages/itemDetails/itemDetails";
 import SearchResult from "./pages/searchResult/searchResult";
-// import NavBar from "./components/navBar/navBar";
-// import Footer from "./components/footer/footer";
 
 import "./fontawesome/css/all.css";
 import "./App.scss";
@@ -34,13 +33,14 @@ const App = props => {
     <BrowserRouter>
       <ScrollToTop />
       <div className="App">
-        {/* <NavBar /> */}
-        <Route path="/" exact component={LandingPage} />
-        <Route path="/discover" component={Discover} />
-        <Route path="/details/:type/:id" component={ItemDetails} />
-        <Route path="/search-results/:id" component={SearchResult} />
-        {/* <Footer /> */}
+        <Switch>
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/discover" component={Discover} />
+          <Route path="/details/:type/:id" component={ItemDetails} />
+          <Route path="/search-results/:id" component={SearchResult} />
+        </Switch>
       </div>
+      <ScrollToTop />
     </BrowserRouter>
   );
 };
