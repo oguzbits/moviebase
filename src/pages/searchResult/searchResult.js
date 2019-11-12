32a3200656bf6517480d9341f2b3854b2c7683cd
Oguz <me@oguzoeztuerk.com>
2019-11-12 21:32:12 +0100

    add fade to searchpage

--- src/pages/searchResult/searchResult.js
+++ src/pages/searchResult/searchResult.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import dayjs from "dayjs";
 
+import Fade from "react-reveal/Fade";
+
 import searchData from "../../actions/searchData";
 
 import NavBar from "../../components/navBar/navBar";
@@ -41,95 +43,100 @@ const SearchResult = props => {
     <div>
       <NavBar />
       <div className="searchresult-main">
-        <header className="searchresult-header">
-          <h1>Search results for "{params}"</h1>
-        </header>
-        <div className="searchresult-body">
-          <hr />
-          <div className="card-wrapper">
-            {props.searchDataResults.results != 0 ? (
-              props.searchDataResults.results.map(
-                item =>
-                  imageSource(item) &&
-                  item.media_type === ("movie" || "tv") && (
-                    <Link
-                      to={`/details/${item.media_type.toLowerCase()}/${
-                        item.id
-                      }`}
-                      key={item.id}
-                      id="card-container"
-                      className="card">
-                      <div id="card-grid">
-                        <div className="card">
-                          <img
-                            src={imageSource(item)}
-                            className="card-img"
-                            alt="..."
-                          />
-                        </div>
-                        <div className="card">
-                          <div className="card-body">
-                            <div className="card-title">
-                              <div>
-                                <CircularProgressbar
-                                  className="circularprogressbar"
-                                  styles={buildStyles({
-                                    textSize: "45px",
-                                    pathColor: `${pathTrailColor(
-                                      item.vote_average
-                                    )}`,
-                                    textColor: "#fff",
-                                    trailColor: "black",
-                                    backgroundColor: "black"
-                                  })}
-                                  strokeWidth={10}
-                                  background
-                                  backgroundPadding={6}
-                                  value={item.vote_average * 10}
-                                  text={
-                                    <tspan dy={2.5}>
-                                      {item.vote_average * 10}
-                                    </tspan>
-                                  }
-                                />
+        <Fade>
+          <header className="searchresult-header">
+            <h1>Search results for "{params}"</h1>
+          </header>
+          <div className="searchresult-body">
+            <hr />
+            <div className="card-wrapper">
+              {props.searchDataResults.results != 0 ? (
+                props.searchDataResults.results.map(
+                  item =>
+                    imageSource(item) &&
+                    item.media_type === ("movie" || "tv") && (
+                      <Link
+                        to={`/details/${item.media_type.toLowerCase()}/${
+                          item.id
+                        }`}
+                        key={item.id}
+                        id="card-container"
+                        className="card"
+                      >
+                        <div id="card-grid">
+                          <div className="card">
+                            <img
+                              src={imageSource(item)}
+                              className="card-img"
+                              alt="..."
+                            />
+                          </div>
+                          <div className="card">
+                            <div className="card-body">
+                              <div className="card-title">
+                                <div>
+                                  <CircularProgressbar
+                                    className="circularprogressbar"
+                                    styles={buildStyles({
+                                      textSize: "45px",
+                                      pathColor: `${pathTrailColor(
+                                        item.vote_average
+                                      )}`,
+                                      textColor: "#fff",
+                                      trailColor: "black",
+                                      backgroundColor: "black"
+                                    })}
+                                    strokeWidth={10}
+                                    background
+                                    backgroundPadding={6}
+                                    value={item.vote_average * 10}
+                                    text={
+                                      <tspan dy={2.5}>
+                                        {item.vote_average * 10}
+                                      </tspan>
+                                    }
+                                  />
+                                </div>
+                                <div>{item.title || item.name}</div>
                               </div>
-                              <div>{item.title || item.name}</div>
+                              <p
+                                className="card-text"
+                                style={
+                                  ({ minHeight: "85px" }, { maxHeight: "85px" })
+                                }
+                              >
+                                {item.overview.length > 200
+                                  ? `${item.overview.substring(0, 200)}...`
+                                  : item.overview}
+                              </p>
                             </div>
-                            <p
-                              className="card-text"
-                              style={
-                                ({ minHeight: "85px" }, { maxHeight: "85px" })
-                              }>
-                              {item.overview.length > 200
-                                ? `${item.overview.substring(0, 200)}...`
-                                : item.overview}
-                            </p>
-                          </div>
-                          <div
-                            id="footer-container"
-                            className={
-                              item.vote_average >= 7
-                                ? "card-footer text-muted border-success"
-                                : "card-footer text-muted border-warning"
-                            }>
-                            <div className="card-footer">
-                              <small className="text-muted">
-                                {dayjs(
-                                  item.release_date || item.first_air_date
-                                ).format("MMMM D, YYYY")}
-                              </small>
+                            <div
+                              id="footer-container"
+                              className={
+                                item.vote_average >= 7
+                                  ? "card-footer text-muted border-success"
+                                  : "card-footer text-muted border-warning"
+                              }
+                            >
+                              <div className="card-footer">
+                                <small className="text-muted">
+                                  {dayjs(
+                                    item.release_date || item.first_air_date
+                                  ).format("MMMM D, YYYY")}
+                                </small>
+                              </div>
                             </div>
                           </div>
                         </div>
-                      </div>
-                    </Link>
-                  )
-              )
-            ) : (
-              <h2 style={{ color: "white" }}>No results found</h2>
-            )}
+                      </Link>
+                    )
+                )
+              ) : (
+                <h2 style={{ color: "white" }}>No results found</h2>
+              )}
+            </div>
           </div>
-        </div>
+        </Fade>
       </div>
     </div>
   );
@@ -145,7 +152,4 @@ const mapDispatchToProps = dispatch => ({
   searchData: url => dispatch(searchData(url))
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(SearchResult);
+export default connect(mapStateToProps, mapDispatchToProps)(SearchResult);
